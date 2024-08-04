cfa8a32f9384a490dc27f8fc21c290d8cd137d6a
Thilagavathy17 <thilsresponsive@gmail.com>
2024-08-04 20:41:43 +0800

    Update LoginFirefox.java

--- src/main/java/com/leaftaps/testcases/LoginFirefox.java
+++ src/main/java/com/leaftaps/testcases/LoginFirefox.java
@@ -32,7 +32,7 @@ public class LoginFirefox {
 		//dc.setVersion("120.0");
 		dc.setPlatform(Platform.LINUX);
 
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.244.37.50:4444/ui/wd/hub"), dc);
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.244.37.50:4444/wd/hub"), dc);
 
 		//ChromeDriver driver = new ChromeDriver();
 
